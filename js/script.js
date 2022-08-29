6f225dd91d63968dce47647cef67ccea6aa06c3b
Nick M <mully7773@gmail.com>
2022-08-29 14:59:36 -0500

    Successfully rendered all characters with their data from single FF series to webpage

--- js/script.js
+++ js/script.js
@@ -8,21 +8,63 @@ const characterDescriptionContainer = document.querySelector(
   ".character-description--container"
 );
 
-// Render one character
-const renderOneCharacter = (data) => {
-  const characterPicture = data[193].pictures[0].url;
-  const characterName = data[193].name;
-  const characterDescription = data[193].description;
-  const characterOrigin = data[193].origin;
-  const characterRace = data[193].race;
-  const characterJob = data[193].job;
-  const characterHeight = data[193].height;
-  const characterAge = data[193].age;
-  const characterWeight = data[193].weight;
-  const characterData = data[193];
+const mainContentEl = document.querySelector(".main-content-grid");
 
-  const html = `<div class="character-profile">
-    <img class="character-img" src="${characterPicture}" />
+// Render one character
+// const renderOneCharacter = (data) => {
+//   const characterPicture = data[193].pictures[0].url;
+//   const characterName = data[193].name;
+//   const characterDescription = data[193].description;
+//   const characterOrigin = data[193].origin;
+//   const characterRace = data[193].race;
+//   const characterJob = data[193].job;
+//   const characterHeight = data[193].height;
+//   const characterAge = data[193].age;
+//   const characterWeight = data[193].weight;
+//   const characterData = data[193];
+
+//   const html = `<div class="character-profile">
+//     <img class="character-img" src="${characterPicture}" />
+//     <div class="character-data-grid">
+//       <p>Origin: ${characterOrigin}</p>
+//       <p>Race: ${characterRace}</p>
+//       <p>Job: ${characterJob}</p>
+//       <p>Height: ${characterHeight}</p>
+//       <p>Age: ${characterAge}</p>
+//       <p>Weight: ${characterWeight}</p>
+//     </div>
+//   </div>`;
+//   characterProfileContainer.insertAdjacentHTML("afterbegin", html);
+
+//   const descHtml = `
+//   <div class="character-description">
+//     <h3>${characterName}</h3>
+//     <p>
+//       ${characterDescription}
+//     </p>
+//   </div>`;
+//   characterDescriptionContainer.insertAdjacentHTML("afterbegin", descHtml);
+// };
+
+const renderFFVII = (data) => {
+  const ffSeven = data.filter((game) => game.origin === "Final Fantasy VII");
+  console.log(ffSeven);
+
+  ffSeven.map((data) => {
+    const charImgs = data.pictures[0].url;
+    const characterName = data.name;
+    const characterDescription = data.description;
+    const characterOrigin = data.origin;
+    const characterRace = data.race;
+    const characterJob = data.job;
+    const characterHeight = data.height;
+    const characterAge = data.age;
+    const characterWeight = data.weight;
+
+    const html = `
+    <article class="character-profile--container">
+    <div class="character-profile">
+    <img class="character-img" src="${charImgs}" />
     <div class="character-data-grid">
       <p>Origin: ${characterOrigin}</p>
       <p>Race: ${characterRace}</p>
@@ -31,17 +73,22 @@ const renderOneCharacter = (data) => {
       <p>Age: ${characterAge}</p>
       <p>Weight: ${characterWeight}</p>
     </div>
-  </div>`;
-  characterProfileContainer.insertAdjacentHTML("afterbegin", html);
-
-  const descHtml = `
-  <div class="character-description">
-    <h3>${characterName}</h3>
-    <p>
-      ${characterDescription}
-    </p>
-  </div>`;
-  characterDescriptionContainer.insertAdjacentHTML("afterbegin", descHtml);
+  </div>
+  </article>
+  
+  <article class="character-description--container">
+          <!-- <div class="character-description">
+            <h3>${characterName}</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi
+              nobis quia corrupti placeat laudantium ratione cumque,
+              necessitatibus deserunt aliquam ad ullam expedita sequi nostrum
+              ducimus error officia iste id quisquam?
+            </p>
+          </div> -->
+        </article>`;
+    mainContentEl.insertAdjacentHTML("afterend", html);
+  });
 };
 
 // Fetch character data
@@ -51,7 +98,7 @@ const getCharacterData = async () => {
     if (!charRes.ok) throw new Error("Problem getting character data");
     const charData = await charRes.json();
     console.log(charData);
-    return renderOneCharacter(charData);
+    return renderFFVII(charData);
   } catch (err) {
     console.error(err);
   }
